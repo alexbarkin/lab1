7f6add7fea9d36bdaef20b590b32defbf5685196
alexbarkin <alexbarkin@rogers.com>
2018-01-23 16:45:55 -0500

    Initial Commit

---
+++ lab1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab1
+//
+//  Created by alex barkin on 2018-01-23.
+//  Copyright © 2018 alex barkin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
